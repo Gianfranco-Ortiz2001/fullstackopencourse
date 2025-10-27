514ef0c09cbb2424a162a63cb89ca8bb253a7236
Gianfranco-Ortiz2001 <gian_ortiz@hotmail.com>
2025-10-27 11:25:58 -0500

    Part 2 exercises 2.1-2.5
    I've been stuck in the exercise 2.5 because I forgot to save Course.jsx before reloading the page, I felt like a total idiot when I finally realized T-T

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Part = ({part}) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => {
+    //console.log(sum, part);
+    return sum + part.exercises
+  }, 0)
+  return (
+    <h4>Total of {total} exercises</h4>
+  )
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+export default Course
