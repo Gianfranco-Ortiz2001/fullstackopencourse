16817cdcaab36e61eca8db64bd0ba9adb4f9e285
Gianfranco-Ortiz2001 <gian_ortiz@hotmail.com>
2026-01-23 14:27:40 -0500

    Exercise 3.9
    Exercise 3.9 and also a long overdue push to the repo

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 morgan.token('body', (req) => {
@@ -109,7 +111,7 @@ app.post('/api/persons', (req, res) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
